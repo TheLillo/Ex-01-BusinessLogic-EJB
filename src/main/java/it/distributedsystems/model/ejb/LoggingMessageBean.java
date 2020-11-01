561e182ad5336ef686e8488c07ece631f7cdf448
Andrea Giovine <andrea.giovine2@gmail.com>
2020-11-01 22:17:29 +0100

    Add message driven bean for logging with JMS

---
+++ src/main/java/it/distributedsystems/model/ejb/LoggingMessageBean.java
@@ -0,0 +1,28 @@
+package it.distributedsystems.model.ejb;
+import javax.ejb.ActivationConfigProperty;
+import javax.ejb.MessageDriven;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.jms.TextMessage;
+
+
+@MessageDriven(activationConfig = {
+        @ActivationConfigProperty(
+                propertyName = "destination",
+                propertyValue = "LoggingQueue"),
+        @ActivationConfigProperty(
+                propertyName = "destinationType",
+                propertyValue = "javax.jms.Queue")
+})
+public class LoggingMessageBean implements MessageListener {
+
+    public void onMessage(Message message) {
+        TextMessage textMessage = (TextMessage) message;
+        try {
+            System.out.println("Message received: " + textMessage.getText());
+        } catch (JMSException e) {
+            System.out.println("Error while trying to consume messages: " + e.getMessage());
+        }
+    }
+}
--- src/main/java/it/distributedsystems/profilers/Profiler.java
+++ src/main/java/it/distributedsystems/profilers/Profiler.java
@@ -1,8 +1,10 @@
 package it.distributedsystems.profilers;
-
-
 import javax.interceptor.AroundInvoke;
 import javax.interceptor.InvocationContext;
+import javax.jms.*;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 
@@ -11,20 +13,37 @@ public class Profiler {
     @AroundInvoke
     public Object profiler(InvocationContext ctx){
         Object obj = null;
-        try {
-            BufferedWriter writer = new BufferedWriter(
-                    new FileWriter("/home/fabbrei/Downloads/log.txt", true));
-            writer.write("creation\n");
-            writer.write(ctx.getMethod().getName()+"\n");
-            writer.write(""+System.currentTimeMillis()+"\n");
-            writer.close();
+        Context ic = null;
+        ConnectionFactory cf = null;
+
+        try{
+            ic = new InitialContext();
+            cf = (ConnectionFactory) ic.lookup("/ConnectionFactory");
+        }catch (NamingException e){
+            e.printStackTrace();
+        }
+
+        try(
+                Connection connection = cf.createConnection();
+                Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+                )
+        {
+            Queue queue = (Queue) ic.lookup("queue/LoggingQueue");
+            MessageProducer publisher = session.createProducer(queue);
+            connection.start();
+            TextMessage message = session.createTextMessage(ctx.getMethod().getName());
+            publisher.send(message);
+        } catch (NamingException | JMSException e) {
+            e.printStackTrace();
+        }
+
+        try{
             obj = ctx.proceed();
         }catch (Exception e){
-
+            e.printStackTrace();
         }
 
         return obj;
-
     }
 
 }
